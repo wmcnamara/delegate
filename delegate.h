d2e2238b689fa4cc80c5985d1dbe2682f07d1118
Weston McNamara <westonfmcnamara@gmail.com>
2020-08-22 00:50:09 -0400

    Update delegate.h

--- delegate.h
+++ delegate.h
@@ -1,62 +1,71 @@
-#ifndef DELEGATE_H
-#define DELEGATE_H
-
-#include <functional>
-#include <map>
-
 /*
 //Weston McNamara 2020
 //Licensed Under MIT https://mit-license.org/
 //https://github.com/wmcnamara/delegate
- 
+
 //Delegate is a single header, lightweight and easy to use abstraction for storing functions and callbacks.
 //When you call AddHandler, a integer type containing the ID is returned. To call RemoveHandler, you must pass this integer.
 //If you know you might delete the handler you add, be sure to keep track of this number.
+//Uses std::map to represent ID/Function data.
 */
 
-namespace Events 
-{
-	template<typename T>
-	class Delegate
-	{
-	private:
-		typedef std::function<void(T)> function;
+#ifndef DELEGATE_INCLUDE
+#define DELEGATE_INCLUDE
 
-	public:
-		//Invokes each function added to this delegate. 
-		//If no handlers exist, it returns false.
-		void Invoke(T param)
-		{
-			if (m_handlers.empty()) 
-			{
-				assert(!m_handlers.empty());
-				std::cout << "Cannot invoke empty vector" << std::endl;
-			}
-
-			for (const auto& key : m_handlers)
-			{
-				key.second(param);
-			}
-		}
-
-		//Adds a single function to the delegate.
-		int AddHandler(function func)
-		{
-			m_handlers.insert(std::pair<int, function>(m_NextID, func));
-
-			//Return the ID, and increment it.
-			return (m_NextID++);
-		}
-
-		//Removes a single function from the delegate
-		void RemoveHandler(int ID) 
-		{
-			m_handlers.erase(ID);
-		}
-		void RemoveAllHandlers() { m_handlers.clear();
-	private:		
-		std::map<int, function> m_handlers;
-		int m_NextID = 0; //Next ID to be used.
-	};
-}
+#ifndef DELEGATE_API
+#define DELEGATE_API //__declspec(dllexport)
 #endif
+
+#ifndef DELEGATE_NOEXCEPT
+#define DELEGATE_NOEXCEPT noexcept //You can omit 'noexcept' here to allow for throwing exceptions.
+#endif
+
+#include <functional>
+#include <map>
+
+template<typename T>
+class Delegate
+{
+private:
+	typedef std::function<void(T)> Func_T;
+	
+public:
+	//Invokes each function added to this delegate. 
+	DELEGATE_API void Invoke(T param) DELEGATE_NOEXCEPT
+	{
+		if (m_handlers.empty())
+			return;
+
+		for (const auto& key : m_handlers)
+		{
+			key.second(param); //Invoke the function
+		}		
+	}
+
+	//Adds a single function to the delegate.
+	DELEGATE_API int AddHandler(Func_T func) DELEGATE_NOEXCEPT
+	{
+		static int nextID = 0;
+
+		m_handlers.insert(std::pair<int, Func_T>(nextID, func));
+
+		//Return the ID, and increment it.
+		return (nextID++);
+	}
+
+	//Removes a single function from the delegate
+	DELEGATE_API void RemoveHandler(int ID) DELEGATE_NOEXCEPT
+	{
+		m_handlers.erase(ID);
+	}
+	
+	//Removes every handler from this delegate.
+	DELEGATE_API void RemoveAllHandlers() DELEGATE_NOEXCEPT
+	{
+		m_handlers.clear();
+	}
+private:
+    std::map<int, Func_T> m_handlers;
+};
+
+#endif //DELEGATE_INCLUDE
